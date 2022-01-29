86ed167d110608539386cc735fc75a02b4b3e50c
Lukas <lukaswehrli32@gmail.com>
2022-01-29 14:38:09 -0600

    Initial Commit

---
+++ BlitzBar/BlitzBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlitzBarApp.swift
+//  BlitzBar
+//
+//  Created by Lukas Wehrli on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct BlitzBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
