a37e357ffb188a85ba84796687793f0a3f03eb40
Lukas <lukaswehrli32@gmail.com>
2022-02-03 13:01:43 -0600

    adding menu icons

--- BlitzBar/MapView.swift
+++ BlitzBar/MapView.swift
@@ -34,10 +34,16 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
 }
 
 struct MapView: View {
-    @StateObject var manager = LocationManager()
-        
+    //@StateObject var manager = LocationManager()
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
     var body: some View {
-        Map(coordinateRegion: $manager.region)
+        VStack{
+            Map(coordinateRegion: $region)
+            //Menu()
+        }
     }
 }
 
---
+++ BlitzBar/Menu.swift
@@ -0,0 +1,33 @@
+//
+//  Menu.swift
+//  BlitzBar
+//
+//  Created by Lukas Wehrli on 2/3/22.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        NavigationView {
+            /*ToolbarContent {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Button("First") {
+                        print("Pressed")
+                    }
+
+                    Button("Second") {
+                        print("Pressed")
+                    }
+                }
+            }
+             */
+        }
+    }
+}
+
+struct Menu_Previews: PreviewProvider {
+    static var previews: some View {
+        Menu()
+    }
+}
